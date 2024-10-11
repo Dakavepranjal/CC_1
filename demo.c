50f34857c679709b74bd5d074204a8c07bd94b88
Pranjal Dakave <120237245+Dakavepranjal@users.noreply.github.com>
2024-10-11 10:15:26 +0530

    demo.c

---
+++ demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n\nThis Program is generated on github platform\n\n");
+    printf("\n\nThis c program exicuted using google app engine\n\n");
+    return 0;
+}
