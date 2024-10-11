c3c1fdac4792dc94ec12cf24242ad73dbd994482
Pranjal Dakave <120237245+Dakavepranjal@users.noreply.github.com>
2024-10-11 10:35:19 +0530

    demo.java

---
+++ demo.java
@@ -0,0 +1,7 @@
+class demo{
+  public static void main(String args[])
+  {
+    System.out.println("This program is generated on Github platform");
+    System.out.println("This java programexicuted using google app engine")
+  }
+}
